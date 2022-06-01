ee0ca2e20bbae89ec728e02306141e15cd7c4523
HarryDevDude <102680902+HarryDevDude@users.noreply.github.com>
2022-06-01 16:39:48 -0400

    first commit for hw

---
+++ dashboard/app.js
@@ -0,0 +1,71 @@
+ReactDOM.render(
+  <h1>Commence Dashboard Creation!</h1>,
+    document.querySelector('main')
+);
+
+// app
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+
+// sidebar
+class Sidebar extends React.Component {
+  render () {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+// reviews
+class Reviews extends React.Component {
+  render () {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+// average rating
+class Rating extends React.Component {
+  render () {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+// sentiment analysis
+class SentimentAnalysis extends React.Component {
+  render () {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+// website visitors
+class WebsiteVisitors extends React.Component {
+  render () {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
