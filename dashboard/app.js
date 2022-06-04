7e6d192342a6e86c70b3122d63e7acca02ec5b7a
HarryDevDude <102680902+HarryDevDude@users.noreply.github.com>
2022-06-03 21:43:06 -0400

    added styling and rendered data through map method

--- dashboard/app.js
+++ dashboard/app.js
@@ -1,71 +1,74 @@
-ReactDOM.render(
-  <h1>Commence Dashboard Creation!</h1>,
-    document.querySelector('main')
-);
+class SideBar extends React.Component {
+  render() {
+      return (
+          <div className="sidebar">
+              <p>Dashboard</p>
+              <p>Widget</p>
+              <p>Reviews</p>
+              <p>Customers</p>
+              <p>Online Analysis</p>
+              <p>Settings</p>
+          </div>
+      )
+  }
+}
 
-// app
+
+class Stats extends React.Component {
+  render() {
+      return (
+          <div className="stats">
+              <p>{this.props.reviews.name}</p>
+              <div className="statbox">{this.props.reviews.count.map(number => <p>{number}</p>)}</div>
+              {/* <p>Name</p>
+              <p>Values</p> */}
+          </div>
+      )
+  }
+}
+
+class Visitors extends React.Component {
+  render() {
+      return (
+          <div className="visitors">
+              <p>Website Visitors</p>
+              <p>821</p>
+              <div className="innerBox"></div>
+          </div>
+      )
+  }
+}
+
+/* class VisitorsInside extends React.Component {
+  render() {
+      return (
+          <div>
+
+          </div>
+      )
+  }
+} */
+// console.log(reviews)
 class App extends React.Component {
-  render () {
-    return (
-      <div>
-        
-      </div>
-    )
+  state = {
+      data: reviews
+  }
+
+  render() {
+      return (
+          <div className="container">
+              <SideBar />
+              {reviews.map(object => <Stats reviews={object}/>)}
+              {/* <Stats reviews={reviews[0]}/>
+              <Stats reviews={reviews[1]}/>
+              <Stats reviews={reviews[2]}/> */}
+              <Visitors />
+          </div>
+      )
   }
 }
 
-
-// sidebar
-class Sidebar extends React.Component {
-  render () {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
-}
-
-// reviews
-class Reviews extends React.Component {
-  render () {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
-}
-
-// average rating
-class Rating extends React.Component {
-  render () {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
-}
-
-// sentiment analysis
-class SentimentAnalysis extends React.Component {
-  render () {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
-}
-
-// website visitors
-class WebsiteVisitors extends React.Component {
-  render () {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
-}
+ReactDOM.render(
+  <App />,
+  document.querySelector('.main')
+);
